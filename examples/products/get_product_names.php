679579febb39d5a22d8407110b18a9346398cc19
Stephan Weikert <weikert@digital-print.net>
2021-12-30 12:34:40 +0100

    Update examples

---
+++ examples/products/get_product_names.php
@@ -0,0 +1,19 @@
+<?php
+
+try {
+    require "../initialize.php";
+
+    $product = $oro->products->get(116);
+
+    echo '<b>' . $product->type . ' ' . $product->id . '</b><br>';
+
+    $names = $product->names();
+
+    foreach ($names as $name) {
+        foreach ($name->attributes as $key => $val) {
+            echo "{$key}: {$val}<br>";
+        }
+    }
+} catch (\Oro\Api\Exceptions\ApiException $e) {
+    echo "API call failed: " . $e->getMessage();
+}
---
+++ examples/users/get_user_roles.php
@@ -0,0 +1,19 @@
+<?php
+
+try {
+    require "../initialize.php";
+
+    $user = $oro->users->get(11);
+    
+    echo '<b>' . $user->type . ' ' . $user->id . '</b><br>';
+
+    $roles = $user->roles();
+
+    foreach ($roles as $role) {
+        foreach ($role->attributes as $key => $val) {
+            echo "{$key}: {$val}<br>";
+        }
+    }
+} catch (\Oro\Api\Exceptions\ApiException $e) {
+    echo "API call failed: " . $e->getMessage();
+}
