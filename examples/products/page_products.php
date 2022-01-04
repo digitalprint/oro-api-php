9f63191984ed70f42ddf77656ea1f8b360b749f4
Stephan Weikert <weikert@digital-print.net>
2022-01-04 10:04:38 +0100

    Update examples

--- examples/products/page_products.php
+++ examples/products/page_products.php
@@ -3,9 +3,10 @@
 try {
     require "../initialize.php";
 
+    echo '<ul>';
+
     $products = $oro->products->page();
 
-    echo '<ul>';
     foreach ($products as $product) {
         echo '<li>';
         echo '<b>' . $product->type . ' ' . $product->id . '</b><br>';
@@ -18,7 +19,24 @@ try {
         }
         echo "</li>";
     }
+
+    $nextProducts = $products->next();
+
+    foreach ($nextProducts as $product) {
+        echo '<li>';
+        echo '<b>' . $product->type . ' ' . $product->id . '</b><br>';
+        foreach ($product->attributes as $key => $val) {
+            if (is_array($val)) {
+                echo $key . ': ' . implode(', ', $val) . '<br>';
+            } else {
+                echo "{$key}: {$val}<br>";
+            }
+        }
+      echo "</li>";
+    }
+
     echo '</ul>';
+
 } catch (\Oro\Api\Exceptions\ApiException $e) {
     echo "API call failed: " . $e->getMessage();
 }
